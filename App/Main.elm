f12994afb788b3f807dd002d007d55be952471e4
Goryudyuma <umaretekyoumade@gmail.com>
2017-09-15 14:24:18 +0900

    add module

---
+++ App/Main.elm
@@ -0,0 +1,67 @@
+module Main exposing (..)
+
+import Html exposing (Html, program)
+import Widget
+
+
+-- Model
+
+type alias AppModel = 
+    { widgetModel : Widget.Model
+    }
+
+initialModel : AppModel
+initialModel = 
+    { widgetModel = Widget.initialModel
+    }
+
+init : ( AppModel, Cmd Msg )
+init = 
+    ( initialModel, Cmd.none )
+
+
+-- Message
+
+type Msg
+    = WidgetMsg Widget.Msg
+
+
+-- View
+
+view : AppModel -> Html Msg
+view model = 
+    Html.div []
+        [ Html.map WidgetMsg( Widget.view model.widgetModel)
+        ]
+
+
+-- Update
+
+update : Msg -> AppModel -> ( AppModel, Cmd Msg )
+update message model =
+    case message of
+        WidgetMsg subMsg ->
+            let
+                ( updatedWidgetModel, widgetCmd ) =
+                    Widget.update subMsg model.widgetModel
+            in
+                ( { model | widgetModel = updatedWidgetModel}, Cmd.map WidgetMsg widgetCmd )
+
+
+-- Subscription
+
+subscriptions : AppModel -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+-- App
+
+main : Program Never AppModel Msg
+main = 
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
